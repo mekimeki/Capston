922b535c054622b09df5390413cd38e2ee0cc40a
KSMING94 <thals3213@naver.com>
2019-04-04 15:40:51 +0900

    update mainpage 2019-04-04-16:40

--- front-end/src/components/mainPage_/Register.vue
+++ front-end/src/components/mainPage_/Register.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-  <v-container fluid grid-list-md>
+  <v-container pt-5 fluid grid-list-md>
     <v-layout row wrap>
-      <v-flex xs12 sm6>
+      <v-flex xs6 sm4>
         <v-card>
           <v-container >
-            <span>장르1.</span>
+            <span>등록한 채널</span>
             <v-layout row wrap>
               <v-flex
                 v-for="n in 9"
@@ -14,18 +14,18 @@
                 <v-card flat tile>
                   <v-img
                     :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
-                    height="150px"
+                    height="100px"
                   ></v-img>
                 </v-card>
               </v-flex>
             </v-layout>
           </v-container>
-        </v-card>
+        </v-card>  
       </v-flex>
-      <v-flex xs12 sm6>
+      <v-flex xs6 sm4>
         <v-card>
           <v-container >
-            <span>장르2.</span>
+            <span>등록한 채널</span>
             <v-layout row wrap>
               <v-flex
                 v-for="n in 9"
@@ -35,14 +35,36 @@
                 <v-card flat tile>
                   <v-img
                     :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
-                    height="150px"
+                    height="100px"
                   ></v-img>
                 </v-card>
               </v-flex>
             </v-layout>
           </v-container>
-        </v-card>
+        </v-card>  
       </v-flex>
+      <v-flex xs6 sm4>
+        <v-card>
+          <v-container >
+            <span>등록한 채널</span>
+            <v-layout row wrap>
+              <v-flex
+                v-for="n in 9"
+                :key="n"
+                xs4
+              >
+                <v-card flat tile>
+                  <v-img
+                    :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+                    height="100px"
+                  ></v-img>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>  
+      </v-flex>
+      
     </v-layout>
   </v-container>
 </template>
@@ -52,5 +74,4 @@ export default {}
 </script>
 
 <style lang="css" scoped>
-
 </style>
